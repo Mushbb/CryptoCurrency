6e5efcef2c2829789608c76cae8bf2b4c988cff9
Mushbb <myajsajs@gmail.com>
2019-01-19 17:49:10 +0900

    190119

--- line.js
+++ line.js
@@ -28,11 +28,14 @@ class Line{
         this.fy_domain = 0;
         this.ly_domain = 100;
 
-        this.r_margin = -100;
+        this.r_margin = 0;
 
         this.dots = [];
         this.dotx = [];
         this.doty = [];
+        //////////////////////////////////////////////////
+        this.dotease = d3.easeExp;
+        //////////////////////////////////////////////////
 
         this.garbage = [];
         this.g_line = [];
@@ -54,6 +57,7 @@ class Line{
 
 		for(let i=0;i<data.length;i++){
 			if( this.data[data[i].id] === undefined ){
+
 				// make new line
 				this.data[data[i].id] = [];
 				this.data[data[i].id].push(data[i].data);
@@ -66,9 +70,12 @@ class Line{
 							.duration(this.duration)
 							.ease(this.easing);
 
+				this.addDot(data[i].id, data[i].data[this.right], data[i].data[this.up], 5);
 			} else {
 				// extend line
 				this.data[data[i].id].push(data[i].data);
+				if(this.data[data[i].id].length >= 3)
+                    this.movDot(data[i].id, this.data[data[i].id][this.data[data[i].id].length-3][this.right], this.data[data[i].id][this.data[data[i].id].length-3][this.up]);
 			}
 			this.data[data[i].id].f = 1;
 		}
@@ -80,19 +87,29 @@ class Line{
 				this.g_line.push(this.lines[this.data[i].id]);
 				this.data.splice(i, 1);
 				this.lines.splice(this.data[i].id, 1);
+
+				this.delDot(this.data[i].id);
 			}
 		}
     }
 
-    addDot(cx, cy, r, color){
-    	let x = this.x, y = this.y;
-
-    	this.dotx.push(cx); this.doty.push(cy);
-    	this.dots.push(this.svg.append("circle")
-			.attr("cx", x(cx)).attr("cy", y(cy))
-			.attr("r", r).style("fill", color)
-		);
+    // 점의 위치도 저장해놔야해
+    addDot(id, cx, cy, r){
+    	this.dotx[id] = cx;
+    	this.doty[id] = cy;
+    	this.dots[id] = this.svg.append("circle")
+            .attr("class", id+"d")
+            .attr("r", r);
 	}
+
+	movDot(id, cx, cy){
+        this.dotx[id] = cx;
+        this.doty[id] = cy;
+    }
+
+    delDot(id){
+        this.dots[id].remove();
+    }
 
     begin(){
         let x = d3.scaleTime()
@@ -111,18 +128,6 @@ class Line{
         this.svg.append("g")
             .attr("class", "y axis")
             .call(d3.axisLeft(y).ticks(10));
-/*
-        for(let i in this.data) {
-            this.svg.append("g")
-                .append("path")
-                    .datum(this.data[i].data)
-                    .attr("class", i)
-                    .transition()
-                    .duration(this.duration)
-                    .ease(this.easing);
-
-
-        }*/
 
         this.x = x; this.y = y;
 
@@ -155,10 +160,17 @@ class Line{
 
         }
 
+        // Redraw the garbages
         for(let i in this.garbage) {
 			this.g_line[this.garbage[i].id].transition().duration(this.duration).ease(this.easing)
 				.attr("d", line(this.garbage[i]));
 		}
+
+        // Redraw the dots
+        for(let i in this.dots){
+            this.dots[i].transition().duration(this.duration).ease(this.dotease)
+                .attr("cx", x(this.dotx[i])).attr("cy", y(this.doty[i]));
+        }
 
         d3.transition(this.svg).select(".x.axis").duration(this.duration).ease(this.easing)
             .call(d3.axisBottom(x).ticks(5));
